7dc581b653319e9ba35f9bb23cb3ea69cb542966
arjun jaiswal <arjunjaiswal4242@gmail.com>
2026-03-31 17:03:43 +0530

    small cors change

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors({
     origin: [
-        "http://localhost:3000",
+        "http://localhost:5173",
         "https://your-vercel-app.vercel.app"
     ],
     credentials: true
